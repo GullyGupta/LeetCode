614ba1d3391b50375d97b3c92f403b66a3f47314
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-06-29 23:36:18 +0530

    Add files via upload

---
+++ Reverse_string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s)
+    {
+        int n=s.size();
+        for(int i=0,j=n-1;i<j;i++,j--)
+        {
+         swap(s[i],s[j]);
+         }
+    }
+};
