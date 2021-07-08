f38bf3f49c0c9356586c417d229a6d8f8fee13fc
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-07-08 22:05:29 +0530

    Add files via upload

---
+++ uglyno.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isUgly(int n)
+    {
+       if(n==1)
+        return 1;
+       if(n==0)
+        return 0;
+    
+    while(n%2==0)
+           n/=2;
+    while(n%3==0)
+           n/=3;
+    while(n%5==0)
+           n/=5;
+        
+        if(n==1)
+            
+        return 1;
+        
+        return 0;
+    
+    }
+};
+        
