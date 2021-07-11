b0e86a3e273f3c6609feaa730179d7589f3172b8
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-07-11 23:50:56 +0530

    Add files via upload

---
+++ Waterbottle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) 
+    {
+        
+        int ans = numBottles;
+        while(numBottles>0 && numBottles>=numExchange){
+            ans+=numBottles/numExchange;
+            numBottles=numBottles/numExchange+numBottles%numExchange;
+        }
+        
+        return ans;
+        
+    }
+};
