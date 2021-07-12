f9021c82a5cd65860a5d933aea58b1050bc23e52
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-07-12 23:34:27 +0530

    Add files via upload

---
+++ largestoddnum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    string largestOddNumber(string num) 
+    {
+        
+         string ans;
+        for(int i=num.size()-1;i>=0;i--)
+        {
+            if((num[i]-'0')&1)
+            {
+                ans=num.substr(0,i+1);
+                break;
+            }
+        }
+        return ans;
+        
+    }
+};
