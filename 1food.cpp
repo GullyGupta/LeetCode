a1ee76f9a70c8d574a4c9ff0bf964e31646c214f
Gulshan Gupta <70631843+GullyGupta@users.noreply.github.com>
2021-08-20 23:48:45 +0530

    Add files via upload

---
+++ 1food.cpp
@@ -0,0 +1,15 @@
+//challenge 1st
+/* Favourite food */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   string foodname, name;
+   cout << "Please enter your good name-" << endl;
+   cin >> name;
+   cout << "Hello, "<< name << ", nice to meet you, What is your favourite food? \n";
+   cin >> foodname;
+   cout << "Great! "<< foodname << " is one of my favourite food\n ";
+   return 0;
+}
